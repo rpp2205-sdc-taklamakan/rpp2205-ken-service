2250227d3c14a0ce6915ae8d6f74f9c64fc2f44a
Ken Kurita <kenkurita@Kens-MacBook-Pro.local>
2022-12-04 19:24:55 -0800

    test

--- server/kenDatabase/index.js
+++ server/kenDatabase/index.js
@@ -1,7 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const port = 3001;
-// const path = require('path');
 require('dotenv').config();
 const path = require('path');
 const axios = require('axios');
@@ -9,38 +5,19 @@ const express = require('express');
 const compression = require('compression');
 const Promise = require("bluebird");
 const cloudinary = require("cloudinary").v2;
-const pgp = require('pg-promise')();
-const db = pgp('postgres://kenkurita:@localhost:5432/questionanswer')
 
 const app = express();
 app.use(compression());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
 const headers = {headers: {authorization: process.env.TOKEN}};
 const root = 'http://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp'
 
-// get all products
-app.get('/products', async (req, res) => {
-  let url = `${root}/products?count=20`;
-  // const products = await axios.get(url, headers);
-  // res.status(200).json(products.data);
-  axios.get(url, headers)
-    .then((products) => {
-      res.status(200).json(products.data);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).send(err);
-    })
-})
-
-
-app.get('/', (req, res) => {
-  res.send('')
-});
-
+/* ---- QA ---- */
 
 app.get('/qa/questions/:product_id', (req, res) => {
   db.query(`SELECT * from Question where productId=641753;`)
@@ -92,6 +69,14 @@ app.get('/qa/questions/:product_id', (req, res) => {
   .catch((err) => console.error(err))
 })
 
+// get questions
+app.get('/qa/questions/:product_id', (req, res) => {
+  let url = `${root}/qa/questions/?product_id=${req.params.product_id}&count=50`;
+  axios.get(url, headers)
+  .then((response) => res.status(200).json(response.data))
+  .catch((err) => console.error(err))
+})
+
 // get answers
 app.get('/qa/questions/:question_id/answers', (req, res) => {
   let url = `${root}/qa/questions/${req.params.question_id}/answers?count=50`;
@@ -139,13 +124,6 @@ app.put('/qa/answers/:answer_id/helpful', (req, res) => {
   .catch((err) => console.error(err))
 })
 
-// report question
-// app.put('/qa/questions/:question_id/report', (req, res) => {
-//   let url = `${root}/qa/questions/${req.params.question_id}/helpful`;
-//   axios.put(url, {}, headers)
-//   .then((response) => res.status(204).json(response.data))
-//   .catch((err) => console.error(err))
-// })
 
 // report answer
 app.put('/qa/answers/:answer_id/report', (req, res) => {
@@ -158,19 +136,14 @@ app.put('/qa/answers/:answer_id/report', (req, res) => {
   })
 })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
 
-// send interactions detail to API
-app.post('/interactions', (req, res) => {
-  let url = `${root}/interactions`;
-  const {element, widget, time} = req.body;
-  console.log('interactions====> ', req.body);
-  return axios.post(url, {element, widget, time}, headers)
-    .then((result) => {
-      console.log('interactions message: ', result.statusText);
-      res.status(result.status).json('just created')
+
+// product reviews meta
+app.get('/reviews/meta/:product_id', (req, res) => {
+  let url = `${root}/reviews/meta?product_id=${req.params.product_id}`;
+  return axios.get(url, headers)
+    .then((results) => {
+      res.status(200).json(results.data);
     })
     .catch(err => {
       console.log(err);
@@ -178,15 +151,19 @@ app.post('/interactions', (req, res) => {
     })
 })
 
-app.get('/:productId', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../client/dist') }, (err) => {
-    if(err) {
-      res.status(500).json(err);
-    } else {
-      console.log('Chang id');
-    }
-  })
 
-})
-let PORT = process.env.PORT || 3001;
+// product
+app.get('/products/:product_id', (req, res) => {
+  let url = `${root}/products/${req.params.product_id}`;
+  return axios.get(url, headers)
+          .then(result => {
+            res.status(200).json(result.data)})
+            .catch(err => {
+              console.log(err);
+              res.status(500).json(err);
+            })
+});
+
+
+let PORT = 3001;
 app.listen(PORT, () => console.log(`Listening at Port: ${PORT}`));
